726567aacae2a073b2bbf3d4bf9f3567ea9ae122
eun1013 <tjddms4248@naver.com>
2025-06-26 17:17:50 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import MainLayout from "./components/MainLayout";
 import Main from "./components/Main";
 import Login from "./components/Login";
 const App = () => {
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Main />} />
           <Route path="login" element={<Login />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    // </BrowserRouter>
   );
 };
 
